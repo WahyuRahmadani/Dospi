57fa64e54ae98704807388cc44c2fa34c7c38c25
BakaDiez <62246777+BakaDiez@users.noreply.github.com>
2020-03-17 21:30:00 +0700

    new shop detail
    hehe

--- application/modules/shop_detail_penjual/controllers/Shop_detail_penjual.php
+++ application/modules/shop_detail_penjual/controllers/Shop_detail_penjual.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Shop_detail extends MX_Controller {
+class Shop_detail_penjual extends MX_Controller {
 
 	function __construct()
 	{
 		parent::__construct();
 		// model
-		 $this->load->model('m_shop_detail');
+		 $this->load->model('m_shop_detail_penjual');
 		//  $this->load->model('login/m_session');
 	}
 
@@ -16,17 +16,11 @@ class Shop_detail extends MX_Controller {
 	function index()
 	{
 		$data = array(
-			'namamodule' 	=> "shop_detail",
-			'namafileview' 	=> "V_shop_detail",
-			'tampil'		=> $this->m_shop_detail->tampil(),
+			'namamodule' 	=> "shop_detail_penjual",
+			'namafileview' 	=> "V_shop_detail_penjual",
+			'tampil'		=> $this->m_shop_detail_penjual->tampil(),
 		);
 		echo Modules::run('template/tampilCore4', $data);
-	}
-
-	function tambah()
-	{
-		$this->m_tambah_hewan->hewan();
-		redirect('Tambah_hewan');
 	}
 
 }
