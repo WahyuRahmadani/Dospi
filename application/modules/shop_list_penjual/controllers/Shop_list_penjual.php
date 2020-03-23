ae2f4e27516322bddb6034e41f4c026de492fa51
amelia-rizky <62246858+amelia-rizky@users.noreply.github.com>
2020-03-23 17:26:36 +0700

    shop
    hmmmmmmmmm

--- application/modules/shop_list_penjual/controllers/Shop_list_penjual.php
+++ application/modules/shop_list_penjual/controllers/Shop_list_penjual.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Shop_list extends MX_Controller {
+class Shop_list_penjual extends MX_Controller {
 
 	function __construct()
 	{
 		parent::__construct();
 		// model
-		 $this->load->model('M_shop_list');
+		 $this->load->model('M_shop_list_penjual');
 
 		 // $this->load->model('login/m_session');
 	}
@@ -17,9 +17,9 @@ class Shop_list extends MX_Controller {
 	function index()
 	{
 		$data = array(
-			'namamodule' 	=> "shop_list",
-			'namafileview' 	=> "V_shop_list",
-			'tampil'		=> $this->M_shop_list->tampil(),
+			'namamodule' 	=> "shop_list_penjual",
+			'namafileview' 	=> "V_shop_list_penjual",
+			'tampil'		=> $this->M_shop_list_penjual->tampil(),
 		);
 		echo Modules::run('template/tampilCore4', $data);
 	}
--- application/modules/shop_list_penjual/models/M_shop_list_penjual.php
+++ application/modules/shop_list_penjual/models/M_shop_list_penjual.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_shop_list extends CI_Model {
+class M_shop_list_penjual extends CI_Model {
 
 	function tampil()
 	{
