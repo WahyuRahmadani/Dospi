6d6b90324f141cf065576bd9f7528256e56706fa
amelia-rizky <62246858+amelia-rizky@users.noreply.github.com>
2020-03-26 21:38:25 +0700

    cara
    coba

--- application/modules/tata_cara_penjual/models/M_tata_cara_penjual.php
+++ application/modules/tata_cara_penjual/models/M_tata_cara_penjual.php
@@ -7,5 +7,19 @@ class M_tata_cara_penjual extends CI_Model {
 	{
 		return $this->db->get('data_hewan')->result();
     }
+    function tata_cara()
+    {
+    	$id_tatacara                = $this->input->post('id_tatacara');
+    	$judul 					    = $this->input->post('judul');
+    	$isi 					    = $this->input->post('konten');
+    	
+    	$cara = array(
+				'judul'				=>$judul,
+				'konten'					=>$isi,
+			);
+
+		$this->db->insert('tb_tatacara',$isi);
+
+    }
     
 }
