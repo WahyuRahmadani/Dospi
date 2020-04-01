12a6902061b4709d1df980e52f932af51418e95a
amelia-rizky <62246858+amelia-rizky@users.noreply.github.com>
2020-04-01 19:27:45 +0700

    coba gambar
    pppppp

--- application/modules/tentang_dospi_penjual/models/M_tentang_dospi_penjual.php
+++ application/modules/tentang_dospi_penjual/models/M_tentang_dospi_penjual.php
@@ -7,16 +7,4 @@ class M_tentang_dospi_penjual extends CI_Model {
 	{
 		return $this->db->get('tb_tentang')->result();
     }
-
-    function tentang()
-    {
-    	$id_tentang = $this->input->post('id_tentang');
-
-		$judul 		= $this->input->post('judul');
-		$konten	 	= $this->input->post('konten');
-		
-		$this->db->insert('tb_tentang',$konten);
-    }
-
-    
 }
--- application/modules/tentang_dospi_penjual/views/V_tentang_dospi_penjual.php
+++ application/modules/tentang_dospi_penjual/views/V_tentang_dospi_penjual.php
@@ -10,6 +10,7 @@
             </ul>
             <?php foreach($tampil as $tentangdospipenjual) {
     $id = $tentangdospipenjual->id_tentang;
+    $gambar = $tentangdospipenjual->gambar;
 ?>
         </div>
     </div>
@@ -23,7 +24,7 @@
         <div class="row">
             <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12">
                 <div class="about-slider-box simple-slider">
-                    <img class="d-block w-100" src="assets/img/about.jpg" alt="about">
+                    <img class="d-block w-100" src="<?php echo base_url ()?>assets/img/<?php echo $tentangdospipenjual->gambar?>" alt="Image">
                 </div>
             </div>
             <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12">
